1662c41d071ec672a0900e4f838cc5dcdf36e5cd
DocDuck <alex.mitin.psmu@gmail.com>
2020-02-12 12:25:30 +0500

    Поменял список товаров в корзине на табличную верстку. Так проще печатать

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,34 +1,30 @@
 <template>
   <v-row justify="center">
-    <v-dialog v-model="isCardOpen" scrollable max-width="300px">
+    <v-dialog v-model="isCardOpen" scrollable max-width="800px">
       <v-card>        
-        <v-list>
-          <v-subheader>ТОВАРЫ</v-subheader>
-          <v-list-item-group v-model="item" color="primary">
-          <v-list-item
-            v-for="(item, i) in products"
-            :key="i"
-            :inactive="inactive"
-          >
-            <v-list-item-avatar v-if="avatar">
-              <styled-icon :color="item.description"></styled-icon>
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title>{{item.name}}</v-list-item-title>
-              <v-list-item-subtitle>{{item.description}}</v-list-item-subtitle>
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-btn icon>
-                <v-icon color="grey lighten-1">убрать товар</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </v-list-item>
-          
-        </v-list-item-group>
-        </v-list>          
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">Изображение</th>
+                <th class="text-left">Товар</th>
+                <th class="text-left">Описание</th>
+                <th class="text-left">Цена</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in products" :key="item.name">
+                <td><styled-icon :color="item.description"></styled-icon></td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.description }}</td>
+                <td>${{ item.price }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
         <v-card-actions>
           <v-btn color="blue darken-1" text @click="isCardOpen = false">Закрыть</v-btn>
-          <v-btn color="blue darken-1" text @click="buy">Купить</v-btn>
+          <v-btn color="blue darken-1" text @click="buy">Распечатать позиции</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
